6411d5dd43074df7d9496002f3919d744192fdbd
Panos Vagenas <35837085+vagenas@users.noreply.github.com>
2024-09-11 14:45:12 +0200

    fix: trigger final release incl. transition notice
    Signed-off-by: Panos Vagenas <35837085+vagenas@users.noreply.github.com>

--- quackling/__init__.py
+++ quackling/__init__.py
@@ -2,3 +2,9 @@
 # Copyright IBM Corp. 2024 - 2024
 # SPDX-License-Identifier: MIT
 #
+
+import warnings
+
+warnings.warn(
+    "This package is deprecated; please instead use Docling (https://github.com/DS4SD/docling)"  # noqa
+)
